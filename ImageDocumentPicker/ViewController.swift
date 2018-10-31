d32672bcf843c5b02898fefe72d0cadfdbd2bd6f
Richard Rodriguez <richardrodz@gmail.com>
2018-10-31 17:25:39 -0500

    Initial Commit

---
+++ ImageDocumentPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageDocumentPicker
+//
+//  Created by Richard Rodriguez on 10/31/18.
+//  Copyright © 2018 Richard Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
